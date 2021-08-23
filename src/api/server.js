5856ca1d81d25e01066a6436d49565f3af8f8275
Ramdoni007 <ramd42156@gmail.com>
2021-08-23 16:43:45 +0700

    Improve comments

--- src/api/server.js
+++ src/api/server.js
@@ -3,8 +3,7 @@ const http = require("http");
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(async (req, res) => {
-  // mengatur request route
-
+  // Create route/endpoint and HTTP verb/method
   if (req.url === "/mail" && req.method === "POST") {
     let body = "";
 
@@ -25,7 +24,7 @@ const server = http.createServer(async (req, res) => {
     );
   }
 
-  // IF NO ROUTE Present
+  // If no route present
   else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Route not found" }));

43566ab9d077f8472381de3d6f47e17d990f051b
Ramdoni007 <ramd42156@gmail.com>
2021-08-23 16:20:48 +0700

    Add Mysql package in my Project And Connect Node.js with Database Mysql

---
+++ src/api/db_config.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "user-tester",
+  password: "",
+});
+
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Yeyy Berhasil Connect");
+});

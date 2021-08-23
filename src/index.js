8ab7680d21378b195f7462d1db1b00382cc5d286
Ramdoni007 <ramd42156@gmail.com>
2021-08-23 14:12:06 +0700

    Add JSON request

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 const readline = require("readline");
+const http = require("http");
+
 const prompt = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+const options = {
+  hostname: "localhost",
+  port: 5000,
+  path: "/mail",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+let data = JSON.stringify({});
+
 prompt.question("To : ", function (to) {
   prompt.question("From : ", function (from) {
     prompt.question("Message : ", function (message) {
-      console.log("To : " + to);
-      console.log("From : " + from);
-      console.log("Message : " + message);
-
+      data = JSON.stringify({ data: { to, from, message } });
       prompt.close();
     });
   });
 });
 
-prompt.on("close", function () {
-  process.exit(0);
+const req = http.request(options, function (res) {
+  console.log("statusCode: " + res.statusCode);
+
+  res.on("data", function (d) {
+    process.stdout.write(d);
+  });
 });
+
+req.on("error", function (error) {
+  console.log(error);
+});
+
+req.write(data);
+req.end();

af88fddff13d3cc283925dc864ac2059c3a3b2ed
Ramdoni007 <ramd42156@gmail.com>
2021-08-23 10:51:04 +0700

    Add readline prompt for: 1. To 2. From 3. Message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+const readline = require("readline");
+const prompt = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+prompt.question("To : ", function (to) {
+  prompt.question("From : ", function (from) {
+    prompt.question("Message : ", function (message) {
+      console.log("To : " + to);
+      console.log("From : " + from);
+      console.log("Message : " + message);
+
+      prompt.close();
+    });
+  });
+});
+
+prompt.on("close", function () {
+  process.exit(0);
+});

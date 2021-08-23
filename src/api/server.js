8d6f1881f79da5b656c3d01010827271d65b4f31
Ramdoni007 <ramd42156@gmail.com>
2021-08-23 14:11:57 +0700

    Berhasil Membuat Post Rest API :)

--- src/api/server.js
+++ src/api/server.js
@@ -1,1 +1,37 @@
-console.log("Hellow Downychan");
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(async (req, res) => {
+  // mengatur request route
+
+  if (req.url === "/mail" && req.method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      console.log(JSON.parse(body));
+    });
+
+    res.writeHead(201, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        status: "Ok",
+        message: "Message successfully sent",
+      })
+    );
+  }
+
+  // IF NO ROUTE Present
+  else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});

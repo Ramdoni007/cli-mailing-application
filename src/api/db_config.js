b50a6932fd3dc8e8164f46f5d0a2fb8310e32ef7
Ramdoni007 <ramd42156@gmail.com>
2021-08-27 16:18:48 +0700

    Create Connection To DataBase

--- src/api/db_config.js
+++ src/api/db_config.js
@@ -4,12 +4,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "user-tester",
   password: "",
-  database: "table_messages",
+  database: "mailing_database",
 });
-
-// db.connect(function (err) {
-//   if (err) throw err;
-//   console.log("Yeyy Berhasil Connect");
-// });
 
 module.exports = db;
